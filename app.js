be3be8e9a01aa8e21eaf5bf239dc91bfa5f810e6
SHOTA GUTERRES <SHOTA@Peripatetica.domain>
2016-10-13 21:09:57 -0700

    trucking along, slowly but surely.  next are the input fields for all the stats

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ $('#firstClick').click(function(event) {
 	$('#workSection').show();
 	$('#date').hide();
 	$('#start').hide();
+
 });
 
 /*function selectSection() {
@@ -22,14 +23,22 @@ $('#section').change(function() {
 			$("#central").hide();
 			$("#northern").hide();
 			$("#southern").show();
+			console.log($(this).attr("value"));
+			//var southSelected=$(this).attr("value")
+			//storing the above as a variable, but how do i access this 
+			//variable in the next button click?  might have to rewrite 
+			//this as a callback, so that I can call the function, store 
+			//the result, and reuse it elsewhere.  
 		} else if($(this).attr("value")=="LS200") {
 			$("#southern").hide();
 			$("#northern").hide();
 			$("#central").show();
+			console.log($(this).attr("value"));
 		} else {
 			$("#southern").hide();
 			$("#central").hide();
 			$("#northern").show();
+			console.log($(this).attr("value"));
 		}
 	})
 });
@@ -39,5 +48,22 @@ $('#thirdClick').click(function(event) {
 	event.preventDefault();
 	$('#stats').show();
 	$('#area').hide();
-})
+	$('#intro').hide();
+	$('#workSection').hide();
+
+	//if s/c/n then this
+	$('#section').find("option:selected").each(function() {
+		if($(this).attr("value")=="LS100") {
+			console.log(south.options[south.selectedIndex].value);
+			$('#selectedArea').append(south.options[south.selectedIndex].value);
+		} else if($(this).attr("value")=="LS200") {
+			console.log(cent.options[cent.selectedIndex].value);
+			$('#selectedArea').append(cent.options[cent.selectedIndex].value);
+		} else {
+			console.log(north.options[north.selectedIndex].value);
+			$('#selectedArea').append(north.options[north.selectedIndex].value);
+		}
+	});
+	
+});
 
